b1f21c88f7ab90fd6a8a35b0a803a4593ed5ed1c
Zi Nguyen <zinguyen@Zis-MacBook-Pro.local>
2018-01-02 14:34:50 -0500

    add deleting todo feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
 //Check off specific todos by clicking
 $("li").click(function(){
-    //if li is gray
-    if ($(this).css("color") === "rgb(128, 128, 128)") {
-        $(this).css({
-            color: "black",
-            textDecoration: "none"
-        });
-    } else {
-        $(this).css({
-            color: "gray",
-            textDecoration: "line-through"
-        });
-    }
+    $(this).toggleClass("completed");
+});
+
+//click on X to delete todo
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        //passing the callback function into fadeOut will make sure that .remove() occurs after fadeOut
+        //keyword this here refer to clicked span's parent, aka li
+        $(this).remove();
+    });
+    event.stopPropagation();
 });
