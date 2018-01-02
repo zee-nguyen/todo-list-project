c3b7354fc38639c20fee29fa5122f4f4825457d9
Zi Nguyen <zinguyen@Zis-MacBook-Pro.local>
2018-01-02 15:00:41 -0500

    include add new todo feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,26 @@
 //Check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
 //click on X to delete todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
         //passing the callback function into fadeOut will make sure that .remove() occurs after fadeOut
         //keyword this here refer to clicked span's parent, aka li
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+//add new todo
+$("input[type='text']").keypress(function(event){
+   if (event.which === 13) {
+       //extract the input text
+        var todoText = $(this).val();
+       //make new li and append to ul
+       $("ul").append("<li><span>X</span> " + todoText + " </li>");
+       //clear input
+       $(this).val("");
+   }
 });
